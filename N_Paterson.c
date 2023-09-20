a39324795113ce6925c3f751c1a15a41d4ea3b37
Nobody <mishasikolenko@mail.ru>
2023-09-20 22:13:07 +0300

    N-Paterson

---
+++ N_Paterson.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+
+const int N = 10;
+
+void critical_section() {
+    printf("AAAAAAAAAAAAAAAAAAAA\n");
+}
+
+struct lock_one {
+    atomic_int last;
+    atomic_int flag;
+};
+
+struct lock {
+    struct lock_one *locks;
+};
+
+int flags_clear(const struct lock_one *lock, int me) {
+    for (size_t i = 0; i < N; ++i) {
+        if (i != me && atomic_load(&lock[i].flag)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void lock_one(struct lock_one *lock, int me) {
+    atomic_store(&lock->flag, 1);
+    atomic_store(&lock->last, me);
+
+    while ((!flags_clear(lock, me)) && atomic_load(&lock->last) == me) {}
+}
+
+void unlock_one(struct lock_one *lock) {
+    atomic_store(&lock->flag, 0);
+}
+
+void lock(struct lock *locks, int me) {
+    for (size_t i = 0; i < N; ++i) {
+        lock_one(&locks->locks[i], me);
+    }
+}
+
+void unlock(struct lock *locks) {
+    for (int i = N - 1; i >= 0; --i) {
+        unlock_one(&locks->locks[i]);
+    }
+}
+
+int main(void) {
+    struct lock locks;
+    locks.locks = (struct lock_one *)malloc(N * sizeof(struct lock_one));
+    
+    for (int thread_id = 0; thread_id < N; ++thread_id) {
+        lock(&locks, thread_id);
+        critical_section();
+        unlock(&locks);
+    }
+
+    free(locks.locks); 
+
+    return 0;
+}
--- Paterson.c
+++ Paterson.c
@@ -16,7 +16,6 @@ void acquire_lock(struct lock *my_lock, int thread_id){
     atomic_store(&my_lock->flag[thread_id], 1);
     atomic_store(&my_lock->turn, thread_id);
 
-    // Busy-wait until it's the thread's turn and the other thread is not in its critical section
     while (atomic_load(&my_lock->flag[1 - thread_id]) && atomic_load(&my_lock->turn) == thread_id) {}
 }
 
@@ -27,8 +26,8 @@ void release_lock(struct lock *my_lock, int thread_id){
 int main(void){
     struct lock my_lock = { {0, 0}, -1 };
 
-    int thread_id = 0;  // 0 for the first thread, 1 for the second thread
-    
+    int thread_id = 0;
+
     acquire_lock(&my_lock, thread_id);
     critical_section();
     release_lock(&my_lock, thread_id);
