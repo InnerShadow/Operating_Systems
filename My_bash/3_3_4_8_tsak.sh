2ca1198108ec183a12f5ffe64db778b16cdac57e
Nobody <mishasikolenko@mail.ru>
2023-10-06 21:52:49 +0300

    Get covers nicliatids

--- My_bash/3_3_4_8_tsak.sh
+++ My_bash/3_3_4_8_tsak.sh
@@ -4,6 +4,13 @@
 grep 'gene_type "protein_coding"' gencode.v25.primary_assembly.annotation.gtf | \
     awk -F '\t' '{if ($3 == "CDS") print $1, $4-1, $5}' OFS='\t' > unmerged_CDS.bed
 
-#Сортируем
+# Сортируем
 sort -k1,1 -k2,2n unmerged_CDS.bed > sorted_CDS.bed
 
+# Мерджим
+bedtools merge -i sorted_CDS.bed > merged_CDS.bed
+
+# Подсчет суммарного количества покрытых нуклеотидов
+total_nucleotides=$(awk '{sum += $3 - $2} END {print sum}' merged_CDS.bed)
+
+echo "total_nucleotides = $total_nucleotides"
