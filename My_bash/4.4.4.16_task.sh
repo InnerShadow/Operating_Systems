b646a1f95a19a2f232731f8714ba0a5764b38e3a
Nobody <mishasikolenko@mail.ru>
2023-10-23 21:39:16 +0300

    Task 4.4.4.16

--- My_bash/4.4.4.16_task.sh
+++ My_bash/4.4.4.16_task.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-amtools view -F 4 Dm_rnaseq.sorted.bam | cut -f 1 | sort | uniq -c | awk -F " " '{print $1}' | sort -n| uniq -c > counts.txt 
-head -1 counts.txt 
-cat counts.txt | awk -F " " '{sum+=$1} END {print sum}'
+fastq-dump --split-3 SRR3146648
+hisat2 -S Dm_rnaseq.sam -x genome -U Dm_rnaseq.fastq
+
+samtools view -b Dm_rnaseq.sam -o Dm_rnaseq.bam
+
+samtools sort Dm_rnaseq.bam -o Dm_rnaseq.sorted.bam
+
+samtools index Dm_rnaseq.sorted.bam
+
+max_intron_length=$(samtools view Dm_rnaseq.sorted.bam | awk '{ print $6 }' | grep -o ".*[^*]*" | cut -c 3- | tr ',' '\n' | sort -nr | head -n 1)
+
+echo $max_intron_length
 
