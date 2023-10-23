18c3c197c6706bf53fd8d9a2ed6f23b9ffd2c490
Nobody <mishasikolenko@mail.ru>
2023-10-23 21:48:12 +0300

    Code clean

--- My_bash/4.4.4.16_task.sh
+++ My_bash/4.4.4.16_task.sh
@@ -8,7 +8,5 @@ samtools sort Dm_rnaseq.bam -o Dm_rnaseq.sorted.bam
 
 samtools index Dm_rnaseq.sorted.bam
 
-samtools index Dm_rnaseq.sorted.bam
-
 samtools view Dm_rnaseq.sorted.bam | awk '{if ($6 ~ /N/) print $6}' | cut -f 2 -d 'N' | sort -n | tail -n 1
 
