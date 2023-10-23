1678cc8f67644676c59e051a57d133a15a54dd38
Nobody <mishasikolenko@mail.ru>
2023-10-23 23:35:00 +0300

    Finalo4ka

--- My_bash/4.4.4.16_task.sh
+++ My_bash/4.4.4.16_task.sh
@@ -4,10 +4,4 @@ samtools view -b Dm_rnaseq.sam -o Dm_rnaseq.bam
 samtools sort Dm_rnaseq.bam -o Dm_rnaseq.sorted.bam
 
 samtools index Dm_rnaseq.sorted.bam
-
-samtools view Dm_rnaseq.sorted.bam | awk '{ split($6, arr, "N"); for (i = 2; i <= NF; i++) { if (arr[i] ~ /^[0-9]+M$/) print arr[i] } }' > intron_lengths.txt
-
-top_10_introns=$(sort -nr intron_lengths.txt | head -n 10)
-
-echo "Top 10:"
-echo "$top_10_introns"
+samtools view Dm_rnaseq.sorted.bam | awk $6 '{ print $6 }' | cat | grep -o ".M[^N]*" | cut -c 3- | sort -nr | less > 1212.txt
