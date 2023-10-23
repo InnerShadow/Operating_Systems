2ed45439c949ce90042a93d578cb5684ee9c39ce
Nobody <mishasikolenko@mail.ru>
2023-10-23 22:56:49 +0300

    Fix it

--- My_bash/4.4.4.16_task.sh
+++ My_bash/4.4.4.16_task.sh
@@ -5,6 +5,9 @@ samtools sort Dm_rnaseq.bam -o Dm_rnaseq.sorted.bam
 
 samtools index Dm_rnaseq.sorted.bam
 
-longest_intron_length=$(samtools view Dm_rnaseq.sorted.bam | awk '{ print $6 }' | cut -d 'N' -f 2 | sort -nr | head -n 1)
+samtools view Dm_rnaseq.sorted.bam | awk '{ split($6, arr, "N"); for (i = 2; i <= NF; i++) { if (arr[i] ~ /^[0-9]+M$/) print arr[i] } }' > intron_lengths.txt
 
-echo "Len: $longest_intron_length"
+top_10_introns=$(sort -nr intron_lengths.txt | head -n 10)
+
+echo "Top 10:"
+echo "$top_10_introns"
